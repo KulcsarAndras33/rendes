29ba9a412514f889120409a66bb96149a5995e63
KulcsarAndras33 <kulcsar.andras3@gmail.com>
2020-01-21 16:00:49 +0100

    Added appointments; calendar view

--- patient.py
+++ patient.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from datetime import datetime
+from datetime import timedelta
 
 
 class HospitalPatient(models.Model):
@@ -44,3 +44,30 @@ class HospitalDoctor(models.Model):
     gender = fields.Selection([('male', 'Male'),('female', 'Female'),('other', 'Other')], default='male', string='Gender')
     doctor_professional_field = fields.Selection([('surgeon', 'Surgeon'), ('cardiologist', 'Cardiologist'), ('psychiatrist', 'Psychiatrist')], string='Professional Field')
     doctor_patients_name = fields.One2many('hospital.patient', 'patient_doctor_name')
+
+
+class Appointment(models.Model):
+    _name = 'hospital.appointment'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Appointment'
+    _rec_name = 'doctor_name'
+
+    doctor_name = fields.Many2one('hospital.doctor', string='Doctor')
+    patient_name = fields.Char('Patient')
+    start_date = fields.Datetime(string="Start")
+    end_date = fields.Datetime(string="End Date", store=True, compute='_get_end_date', readonly=True)
+    duration = fields.Integer(string="Minutes")
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+            duration = timedelta(minutes=r.duration)
+            r.end_date = r.start_date + duration
+
+    @api.depends('doctor_name', 'patient_name')
+    def _get_name(self):
+        for r in self:
+            r.label_name = r.doctor_name + ": " + r.patient_name
